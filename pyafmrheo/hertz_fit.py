3a7ce8a2140f30cb9dfdb1a0b90d7d824f070522
Javier <jla.lopez.18@gmail.com>
2022-03-18 16:14:00 +0100

    Added fit line option for hertzfit

--- pyafmrheo/hertz_fit.py
+++ pyafmrheo/hertz_fit.py
@@ -5,21 +5,19 @@ from .models.hertz import hertz_model
 
 def HertzFit(
     approach_indentation, approach_force, ind_shape, tip_parameter,
-    p0, poisson_ratio=0.5, sample_height=None, bec_model=None):
+    p0, poisson_ratio=0.5, fit_line=False, sample_height=None, bec_model=None):
 
     params = Parameters()
 
     # Define varying parameters for the hertz fit
     params.add('delta0', value=p0[0])
     params.add('f0', value=p0[1])
-    params.add('slope', value=p0[2])
+    params.add('slope', value=p0[2], vary=fit_line)
     params.add('E0', value=p0[3], min=0)
 
     fixed_params = {
         'poisson_ratio': poisson_ratio, 'indenter_shape': ind_shape,
-        'tip_parameter':tip_parameter, 'sample_height': sample_height,
-        'bec_model': bec_model
-    
+        'tip_parameter':tip_parameter, 'sample_height': sample_height, 'bec_model': bec_model
     }
 
     funchertz = Model(lambda indentation, delta0, E0, f0, slope: hertz_model(indentation, delta0, E0, f0, slope, **fixed_params))
