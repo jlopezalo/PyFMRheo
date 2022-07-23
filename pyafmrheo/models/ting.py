566de2bd7e8968a1b79d6ef0708f7b5a8dac9af7
Javier <Javier.LOPEZ-ALONSO@ibl.cnrs.fr>
2022-07-23 14:23:31 +0200

    test

--- pyafmrheo/models/ting.py
+++ pyafmrheo/models/ting.py
@@ -201,7 +201,7 @@ class TingModel:
         p0 = [self.E0_init, self.tc_init, self.betaE_init,self.F0_init]
         bounds = [
             [self.E0_init*0.001, 0, 0, -100e-12],
-            [self.E0_init*1e5, tm, 1, 100e12]
+            [self.E0_init*1e5, tm, 1, 100e-12]
         ]
         fixed_params = {
             't0': self.t0,
