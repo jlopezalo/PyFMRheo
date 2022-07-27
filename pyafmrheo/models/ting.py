d1cf68cdbf3b77569e94e0112ad9e401e2ea99ee
Javier <Javier.LOPEZ-ALONSO@ibl.cnrs.fr>
2022-07-27 13:20:00 +0200

    test

--- pyafmrheo/models/ting.py
+++ pyafmrheo/models/ting.py
@@ -188,7 +188,7 @@ class TingModel:
         FrNC=F0*np.ones(idxNCr.size)
         # Concatenate non contact regions to the contact region. And return.
         # output =  np.r_[FtNC+v0t*vdrag, FJ, FrNC-v0r*vdrag]
-        output = np.r_[FtNC, FJ, FrNC]+smoothM(numdiff(delta)*vdrag/numdiff(time), 21)
+        output = np.r_[FtNC, FJ+F0, FrNC]+smoothM(numdiff(delta)*vdrag/numdiff(time), 21)
         return output
     
     def fit(self, time, F, delta, t0, idx_tm=None, smooth_w=None, v0t=None, v0r=None):
